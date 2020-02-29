f85d7bc0e6a09ef7085c498ed5ca3d548dcb1e84
Muhanad Muneer <mnieer@msn.com>
2020-02-29 07:00:10 +0200

    Initial commit

---
+++ app/src/main/java/com/childs/childsapp/MainActivity.java
@@ -0,0 +1,113 @@
+package com.childs.childsapp;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import com.childs.session.SessionManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    GridLayout mainGrid;
+    SessionManager sessionManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //initilize session
+        sessionManager = new SessionManager(this);
+
+        //get icon grids
+        mainGrid = (GridLayout) findViewById(R.id.mainGrid);
+
+        //Set Event
+        setSingleEvent(mainGrid);
+        //setToggleEvent(mainGrid);
+    }
+
+    private void setToggleEvent(GridLayout mainGrid) {
+        //Loop all child item of Main Grid
+        for (int i = 0; i < mainGrid.getChildCount(); i++) {
+            //You can see , all child item is CardView , so we just cast object to CardView
+            final CardView cardView = (CardView) mainGrid.getChildAt(i);
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (cardView.getCardBackgroundColor().getDefaultColor() == -1) {
+                        //Change background color
+                        cardView.setCardBackgroundColor(Color.parseColor("#FF6F00"));
+                        Toast.makeText(MainActivity.this, "State : True", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        //Change background color
+                        cardView.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
+                        Toast.makeText(MainActivity.this, "State : False", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void setSingleEvent(GridLayout mainGrid) {
+        //Loop all child item of Main Grid
+        for (int i = 0; i < mainGrid.getChildCount(); i++) {
+            //You can see , all child item is CardView , so we just cast object to CardView
+            CardView cardView = (CardView) mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Intent intent = new Intent(MainActivity.this,ActivityOne.class);
+                    intent.putExtra("info","This is activity from card item index  "+finalI);
+                    startActivity(intent);
+
+                }
+            });
+        }
+    }
+
+    //this custom dialog to choose selected language bu the user
+    private void shoLanguagesDialog() {
+
+        // custom dialog
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.choose_language_dialog);
+        List<String> stringList=new ArrayList<>();  // here is list
+        for(int i=0;i<2;i++) {
+            if (i==0){
+                stringList.add(getResources().getString(R.string.ar_lang));
+            }else {
+                stringList.add(getResources().getString(R.string.en_lang));
+            }
+        }
+
+        //create languages radio buttons
+        RadioGroup rg = (RadioGroup) dialog.findViewById(R.id.langs_radio_group);
+
+        for(int i=0;i<stringList.size();i++){
+            RadioButton rb=new RadioButton(this); // dynamically creating RadioButton and adding to RadioGroup.
+            rb.setText(stringList.get(i));
+            rg.addView(rb);
+
+            
+        }
+    }
+}
